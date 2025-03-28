491cb8b6b2b4fc27610299cf1437c18a21701c5c
antoniofelices <antonio@studiomoare.com>
2025-03-28 19:32:03 +0100

    remove classes, add scss buttons, conteiners, tabs, card

--- assets/js/plugins/tabs.js
+++ assets/js/plugins/tabs.js
@@ -1,6 +1,6 @@
 const tabs = () => {
-    const menuTabsButtons = document.querySelectorAll('.mf-menu-tabs button')
-    const contentTabDiv = document.querySelectorAll('.mf-content-tab')
+    const menuTabsButtons = document.querySelectorAll('.menu-tabs button')
+    const contentTabDiv = document.querySelectorAll('.content-tab')
 
     menuTabsButtons.forEach(button => {
         button.addEventListener('click', function(event) {
@@ -8,19 +8,17 @@ const tabs = () => {
             const tabName = currentButton.getAttribute('data-tab')
 
             currentButton.classList.add('tab-active')
-            currentButton.classList.add('mf-color-blue-900')
             menuTabsButtons.forEach(item => {
                 if (item !== currentButton) {
                     item.classList.remove('tab-active')
-                    item.classList.remove('mf-color-blue-900')
                 }
             })
 
             contentTabDiv.forEach(item => {
-                item.classList.remove('mf-show')
+                item.classList.remove('show')
             })
 
-            document.getElementById(tabName).classList.add('mf-show')
+            document.getElementById(tabName).classList.add('show')
         })
     })
 }
