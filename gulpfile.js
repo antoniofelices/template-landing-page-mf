86d1bda403e20b8f5a5d324ad917cc334b5cee8d
antoniofelices <antonio@studiomoare.com>
2025-03-28 17:12:26 +0100

    first commit scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,15 @@
-const { src, dest, watch, series } = require('gulp');
-const concat = require('gulp-concat');
+const { dest, watch, src, series } = require('gulp');
+// const concat = require('gulp-concat');
+const sass = require('gulp-sass')(require('sass'));
 
 function styles() {
-    return src([
-        'assets/css/mf/preflight.css',
-        'assets/css/mf/fonts.css',
-        'assets/css/mf/variables.css',
-        'assets/css/mf/global.css',
-        'assets/css/mf/type.css',
-        'assets/css/mf/colors.css',
-        'assets/css/mf/spacing.css',
-        'assets/css/mf/utilities.css',
-        'assets/css/mf/shapes.css',
-        'assets/css/mf/grid.css',
-        'assets/css/mf/flex.css',
-        'assets/css/mf/borders.css',
-        'assets/css/mf/border-radius.css',
-        'assets/css/mf/shadows.css',
-        // 'assets/css/custom-variables.css',
-        'assets/css/custom.css'
-    ])
-    .pipe(concat('main.css'))
+    return src('./assets/scss/main.scss')
+    .pipe(sass.sync().on('error', sass.logError))
     .pipe(dest('assets/css'));
 }
 
 function watchFiles() {
-    watch('assets/css/mf/*.css', styles);
+    watch('assets/scss/plugins/*.scss', styles);
 }
 
 exports.default = series(styles, watchFiles);
