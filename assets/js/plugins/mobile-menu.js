08ef57dc4103547cdd0f99a3b8dbf949275aaf5b
antoniofelices <antonio@studiomoare.com>
2025-04-01 16:48:46 +0200

    remove dead code

--- assets/js/plugins/mobile-menu.js
+++ assets/js/plugins/mobile-menu.js
@@ -3,29 +3,15 @@ const mobileMenu = () => {
     const body = document.body;
     const mainHeader = document.getElementById('header');
     const containerNavHeader = document.getElementById('container-nav-header');
-    // const iconClose = document.getElementById('icon-close');
-    // const iconOpen = document.getElementById('icon-open');
     const mobileDropdownToggleButton = document.getElementById('mobile-dropdown-toggle');
     
     if(!containerNavHeader) return;
 
     function toggleMobileMenu() {
-        // body.classList.toggle('h-dvh');
         body.classList.toggle('overflow-y-hide');
         mainHeader.classList.toggle('menu-active');
-
         containerNavHeader.classList.toggle('sm-hide');
-        // iconClose.classList.toggle('mf-sm-hide');
-        // iconOpen.classList.toggle('mf-sm-hide');
     }
-
-    // function closeMobileMenu() {
-    //     body.classList.remove('h-dvh');
-    //     body.classList.remove('mf-overflow-y-hide');
-    //     containerNavHeader.classList.add('mf-sm-hide');
-    //     iconClose.classList.add('mf-sm-hide');
-    //     iconOpen.classList.remove('mf-sm-hide');
-    // }
 
     // function handleOutsideClick(event) {
     //     const target = event.target;
