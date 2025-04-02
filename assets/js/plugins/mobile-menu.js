78c85629e4d456dc6bfb596f1d174a3dd90c3cb1
antoniofelices <antonio@studiomoare.com>
2025-04-02 20:00:15 +0200

    menu mobile

--- assets/js/plugins/mobile-menu.js
+++ assets/js/plugins/mobile-menu.js
@@ -1,43 +1,44 @@
 const mobileMenu = () => {
 
-    const body = document.body;
-    const mainHeader = document.getElementById('header');
-    const containerNavHeader = document.getElementById('container-nav-header');
-    // const iconClose = document.getElementById('icon-close');
-    // const iconOpen = document.getElementById('icon-open');
-    const mobileDropdownToggleButton = document.getElementById('mobile-dropdown-toggle');
+    const body = document.body
+    const header = document.getElementById('header')
+    const logo = document.getElementById('logo-bookmark')
+    const containerNavHeader = document.getElementById('container-nav-header')
+    const mobileDropdownToggleButton = document.getElementById('mobile-dropdown-toggle')
+    const openPathIcon = document.querySelector('#mobile-dropdown-toggle .open')
+    const closePathIcon = document.querySelector('#mobile-dropdown-toggle .close')
     
     if(!containerNavHeader) return;
 
     function toggleMobileMenu() {
-        // body.classList.toggle('h-dvh');
-        body.classList.toggle('overflow-y-hide');
-        mainHeader.classList.toggle('menu-active');
+        body.classList.toggle('overflow-y-hidden')
+        logo.classList.toggle('brightness-1000')
+        header.classList.toggle('menu-active')
 
-        containerNavHeader.classList.toggle('sm-hide');
-        // iconClose.classList.toggle('mf-sm-hide');
-        // iconOpen.classList.toggle('mf-sm-hide');
+        containerNavHeader.classList.toggle('hidden')
+        openPathIcon.classList.toggle('hidden')
+        closePathIcon.classList.toggle('hidden')
     }
 
     // function closeMobileMenu() {
-    //     body.classList.remove('h-dvh');
-    //     body.classList.remove('mf-overflow-y-hide');
-    //     containerNavHeader.classList.add('mf-sm-hide');
-    //     iconClose.classList.add('mf-sm-hide');
-    //     iconOpen.classList.remove('mf-sm-hide');
+    //     body.classList.remove('h-dvh')
+    //     body.classList.remove('mf-overflow-y-hide')
+    //     containerNavHeader.classList.add('mf-sm-hide')
+    //     iconClose.classList.add('mf-sm-hide')
+    //     iconOpen.classList.remove('mf-sm-hide')
     // }
 
     // function handleOutsideClick(event) {
-    //     const target = event.target;
+    //     const target = event.target
     //     if (!target.closest('#container-nav-header') && !target.closest('.mobile-dropdown-toggle')) {
     //         if (!containerNavHeader.classList.contains('mf-sm-hide')) {
-    //             closeMobileMenu();
+    //             closeMobileMenu()
     //         }
     //     }
     // }
 
     mobileDropdownToggleButton.addEventListener('click', toggleMobileMenu);
-    // document.addEventListener('click', handleOutsideClick);
+    // document.addEventListener('click', handleOutsideClick)
 
 }
 
